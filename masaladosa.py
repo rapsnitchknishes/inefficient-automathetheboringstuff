5c245f9770301d48c22900f2f9939903c16aacaf
mridul <mridulsingh4@protonmail.com>
2025-09-15 15:58:39 +0530

    swordfish but masaladosa as password

---
+++ masaladosa.py
@@ -0,0 +1,16 @@
+while True :
+    name = input("enter your name: ")
+    if name == "mridul":
+        print("thank you!")
+        break
+    else:
+        print("sybau don't come here again")
+
+
+while True :
+    password = input("enter your password:")
+    if password == "masaladosa":
+        print("access granted.")
+        break
+    elif password != "masaladosa" :
+        print("you probably know me, so im gonna be a little polite here, syau and don't come here again")
